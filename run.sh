c6b6c46c13a9c1317c9a9a89e605205bd21f267d
Yanko Malinov <yanko.malinov@gmail.com>
2020-03-29 12:10:13 +0300

    Ensure all project dependencies are met when starting app

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # The following file is run at each Raspberry boot-up. Wait a bit to ensure
-# that LXDE has loaded.
+# that LXDE has fully loaded.
 sleep 3
 export RASP_ENV='1'
+
+pip3 install -r /home/pi/src/room-dash/requirements.txt
 /usr/bin/python3 /home/pi/src/room-dash/main.py
