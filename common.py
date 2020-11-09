6bbbd031a40aefb8ebb5a6ad450721fd5722b635
Yanko Malinov <yanko.malinov@gmail.com>
2020-11-09 10:10:56 +0200

    Update schedule

--- common.py
+++ common.py
@@ -20,8 +20,8 @@ routine = routine(
     schedule = [
         # article(time('07:50'), time('09:00')),
         # article(time('20:00'), time('22:30')),
-        article(time('08:00'), time('10:15')),
-        article(time('19:15'), time('20:00')),
+        article(time('08:00'), time('10:05')),
+        article(time('21:00'), time('23:10')),
     ],
     weekdays = {
         MON: True,
